2643c9f639109e2c12df447fa9bb3538bfeedad0
alexjons987 <alexander.jonsson987@outlook.com>
2025-10-27 11:48:50 +0100

    Add Database class

---
+++ jdbc/ReportSystemSchool/src/Database.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+    static final String DB_URL = "jdbc:mysql://localhost:3306/school_db";
+    static final String DB_USER_NAME = "jdbctest_user";
+    static final String DB_USER_PASSWORD = System.getenv("MYSQL_PASSWORD");
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER_NAME, DB_USER_PASSWORD);
+    }
+}
